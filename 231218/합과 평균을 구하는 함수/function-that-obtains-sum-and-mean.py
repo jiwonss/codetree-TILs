e25b241f1479141f42e7144a7f29f12293428655
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-12-18 19:58:56 +0900

    Title: 합과 평균을 구하는 함수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231218/합과 평균을 구하는 함수/function-that-obtains-sum-and-mean.py
+++ 231218/합과 평균을 구하는 함수/function-that-obtains-sum-and-mean.py
@@ -1,7 +1,7 @@
-a, b, c = map(float, input().split())
+a, b, c = map(round, map(float, input().split()))
 
 total = a + b + c
 avg = total // 3
 
-print(round(total))
-print(round(avg))
+print(total)
+print(avg)
