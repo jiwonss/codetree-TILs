d837c0cfc612097377f8a6f92e7f2c2ecce86cfe
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-12-21 01:09:43 +0900

    Title: 물건 옮기기, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

--- 231221/물건 옮기기/moving0-things.py
+++ 231221/물건 옮기기/moving0-things.py
@@ -1,13 +1,13 @@
 n = int(input())
 
-status = {}
-result = 0
+status = [[-1] for _ in range(11)]
 for _ in range(n):
     num, loc = map(int, input().split())
-    if status.get(num):
-        if status[num] == loc:
-            continue
-        result += 1
-    else:
-        status[num] = loc
+    if status[num][-1] != loc:
+        status[num].append(loc)
+
+result = 0
+for s in status:
+    if len(s[1:]) > 0:
+        result += len(s[1:]) - 1
 print(result)
