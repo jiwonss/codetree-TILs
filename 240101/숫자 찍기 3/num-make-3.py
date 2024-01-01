61c37aeaeec38c9b5512536e9ccfa91e8c161efb
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-01-01 22:38:29 +0900

    Title: 숫자 찍기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240101/숫자 찍기 3/num-make-3.py
@@ -0,0 +1,34 @@
+n, t = map(int, input().split())
+
+# 파스칼의 삼각형 만들기
+pascal = [[0 for _ in range(n + 1)] for _ in range(n)]
+pascal[0][0] = 1
+pascal[1][0], pascal[1][1] = 1, 1
+for i in range(2, n):
+    for j in range(n):
+        pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+
+result = [[0 for _ in range(n)] for _ in range(n)]
+if t == 1:
+    for i in range(n):
+        for j in range(n):
+            result[i][j] = pascal[i][j]
+if t == 2:
+    for i in range(n):
+        for j in range(n):
+            result[i][j] = pascal[n - i - 1][j]
+if t == 3:
+    for i in range(n):
+        for j in range(n):
+            result[i][j] = pascal[n - j - 1][n - i - 1]
+
+# 출력
+for i in range(n):
+    if t == 2:
+        print(" " * i, end='')
+    for j in range(n):
+        if result[i][j] == 0:
+            print(" ", end=' ')
+        else:
+            print(result[i][j], end=' ')
+    print()
