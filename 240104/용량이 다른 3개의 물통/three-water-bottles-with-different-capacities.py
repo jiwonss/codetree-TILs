d0935fbfb8f635de09b0c0b26bf2cd71ceb5d4c7
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-01-04 22:56:54 +0900

    Title: 용량이 다른 3개의 물통, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240104/용량이 다른 3개의 물통/three-water-bottles-with-different-capacities.py
@@ -0,0 +1,17 @@
+def move(idx):
+    a, b = idx
+    if info[a][1] + info[b][1] <= info[b][0]:
+        info[b][1] += info[a][1]
+        info[a][1] = 0
+    else:
+        info[b][1] = info[b][0]
+        info[a][1] = info[a][1] - info[b][0] 
+
+info = [list(map(int, input().split())) for _ in range(3)]
+
+idx = [[0, 1], [1, 2], [2, 0]]
+for i in range(100): 
+    move(idx[i % 3])
+
+for i in range(3):
+    print(info[i][1])
