0943890d611329e9cd93ca7356f4f974a311ccd3
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-01-08 23:57:32 +0900

    Title: 3개씩 묶기, Time: 143ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240108/3개씩 묶기/bundle-by-3.py
+++ 240108/3개씩 묶기/bundle-by-3.py
@@ -3,5 +3,8 @@ arr = sorted(list(map(int, input().split())))
 
 result = 0
 for i in range(0, n, 3):
-    result += sum(arr[i:i + 3][1:])
+    if i + 3 - 1 < n:
+        result += sum(arr[i:i + 3][1:])
+    else:
+        result += sum(arr[i:])
 print(result)
