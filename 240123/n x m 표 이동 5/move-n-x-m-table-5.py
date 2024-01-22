6ded858ce52f4e7e7767b84125e08595dd0a6ea7
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-01-23 00:10:11 +0900

    Title: n x m 표 이동 5, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240123/n x m 표 이동 5/move-n-x-m-table-5.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+
+def bfs():
+    q = deque()
+    visited = [[0] * m for _ in range(n)]
+    
+    q.append([0, 0])
+    visited[0][0] = 1
+    
+    while q:
+        x, y = q.popleft()
+        if x == n - 1 and y == m - 1:
+            return visited[x][y]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if visited[nx][ny] or board[nx][ny] == 0:
+                continue
+            visited[nx][ny] = visited[x][y] + 1
+            q.append([nx, ny])
+
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+print(bfs())
