6ef53bbb06aac170a07b8822c06fc44ca6ff3121
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-01-27 22:37:55 +0900

    Title: 진법 변환 7, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240127/진법 변환 7/base-conversion-7.py
@@ -0,0 +1,32 @@
+def calc(num):
+    carry, result = 0, ''
+    for n in num[::-1]:
+        temp = int(n) * 2
+        if carry:
+            temp += carry
+            carry = 0
+        if temp >= 10:
+            carry = temp // 10
+            result += str(temp % 10)
+        else:
+            result += str(temp)
+    if carry:
+        result += str(carry)
+    return result[::-1]
+
+num = input().split('.')
+n, m = int(num[0]), num[1]
+
+integer_result = bin(n)[2:]
+decimal_result = ''
+for _ in range(4):
+    temp = calc(m)
+    if len(m) == len(temp):
+        decimal_result += '0'
+        m = temp
+    else:
+        m = temp[1:]
+        decimal_result += '1'
+
+result = integer_result + '.' + decimal_result
+print(result)
