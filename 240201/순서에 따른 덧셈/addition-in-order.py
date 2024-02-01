43ad405b9f3a35d0774f7e29581f5a27d14adfed
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-02-01 22:33:06 +0900

    Title: 순서에 따른 덧셈, Time: 5059ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 240201/순서에 따른 덧셈/addition-in-order.py
+++ 240201/순서에 따른 덧셈/addition-in-order.py
@@ -1,12 +1,20 @@
+from itertools import permutations
+
+def calc():
+    result = 0
+    for i in range(n):
+        if i - 1 < 0:
+            temp = arr[i] + (i - 1)
+        else:
+            temp = arr[i] - (i - 1)
+        if temp > 0:
+            result += temp
+    return result
+
 n = int(input())
 arr = list(map(int, input().split()))
 
-result = 0
-for i in range(n):
-    if i - 1 < 0:
-        temp = arr[i] + (i - 1)
-    else:
-        temp = arr[i] - (i - 1)
-    if temp > 0:
-        result += temp
+result = 1e9
+for p in permutations(arr, n):
+    result = min(result, calc())
 print(result)
